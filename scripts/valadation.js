82acdc9a360d45cf8ff26736b0958ea6c85bf769
danzell crowell <danzell.crowell94@gmail.com>
2024-10-07 12:04:07 -0400

    updating project

---
+++ scripts/valadation.js
@@ -0,0 +1,33 @@
+console.log("hello from valadation.js");
+
+function setEventListeners(formEl, config) {
+  const { inputSelector } = config;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      console.log(inputEl.valadationMessage);
+    });
+  });
+}
+
+function enableValidation(config) {
+  const formEls = [...document.querySelectorAll(config.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, config);
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
