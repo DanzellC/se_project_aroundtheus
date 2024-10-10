2c8daab5fb5cc40213f63eaa55c1192fe5f67b44
danzell crowell <danzell.crowell94@gmail.com>
2024-10-10 12:54:11 -0400

    update project

--- scripts/valadation.js
+++ scripts/valadation.js
@@ -1,11 +1,28 @@
 console.log("hello from valadation.js");
 
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.valadationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, config) {}
+
+function checkInputValidity(formEl, inputEl, config) {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, config);
+  } else {
+    hideInputError(formEl, inputEl, config);
+  }
+}
+
 function setEventListeners(formEl, config) {
   const { inputSelector } = config;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
-      console.log(inputEl.valadationMessage);
+      checkInputValidity(formEl, inputEl, config);
     });
   });
 }
@@ -25,9 +42,9 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
+  inactiveButtonClass: ".modal__button_disabled",
+  inputErrorClass: ".modal__error",
+  errorClass: ".modal__error_visible",
 };
 
 enableValidation(config);
