a975808ed09822186c5dd0bc379cfd56dedd82e9
DanzellC <danzell.crowel94@gmail.com>
2024-12-22 15:05:04 -0500

    created javascript classes and used webpack for rendering and optimization

---
+++ src/pages/components/popup.js
@@ -0,0 +1,38 @@
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  // open popup
+  open() {
+    this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  // close popup
+  close() {
+    this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  // esc button to close pop up
+  _handleEscClose() {
+    if (e.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    const closeButton = this._popupElement.querySelector(".modal__close");
+    closeButton.addEventListener("click", () => {
+      this.close();
+    });
+
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      if (evt.target.classList.contains("modal")) {
+        this.close();
+      }
+    });
+  }
+}
